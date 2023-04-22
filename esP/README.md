e50a603e131922841cd2dfb983dae96db6f22d0b
YaroslavZuiev <84410015+YaroslavZuiev@users.noreply.github.com>
2023-04-22 19:48:37 +0200

    ter

--- esP/README.md
+++ esP/README.md
@@ -1,4 +1,4 @@
-# APP
+# EsP
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.18.
 
--- esP/karma.conf.js
+++ esP/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/APP'),
+      dir: require('path').join(__dirname, './coverage/esP'),
       subdir: '.',
       reporters: [
         { type: 'html' },
