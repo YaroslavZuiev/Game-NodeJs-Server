c5886ce5f5cc0779f9fe948d140f6f3dd1c01c4e
YaroslavZuiev <84410015+YaroslavZuiev@users.noreply.github.com>
2023-05-20 13:47:25 +0200

    update

---
+++ whale/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./app-layout/app-layout.module').then(({ AppLayoutModule }) => AppLayoutModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
