74555f1d58243f37fdae406443b8d3d89f503a92
YaroslavZuiev <84410015+YaroslavZuiev@users.noreply.github.com>
2023-05-20 13:55:39 +0200

    update

--- babyWhale/src/app/app.component.spec.ts
+++ babyWhale/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'whale'`, () => {
+  it(`should have as title 'babyWhale'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('whale');
+    expect(app.title).toEqual('babyWhale');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('whale app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('babyWhale app is running!');
   });
 });
--- babyWhale/src/app/app.module.ts
+++ babyWhale/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppLayoutComponent } from './app-layout/app-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppLayoutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
