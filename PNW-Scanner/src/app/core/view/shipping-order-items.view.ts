e15aae72f418c71ccbe841b21a34cb631fdee7fa
YaroslavZuiev <yaroslav19071992@gmail.com>
2024-05-23 17:28:17 +0200

    merge

--- PNW-Scanner/src/app/core/view/shipping-order-items.view.ts
+++ PNW-Scanner/src/app/core/view/shipping-order-items.view.ts
@@ -1,34 +1,11 @@
 import {IParsable} from "../models/i-parsable";
+import {ShippingOrderItem} from "../models/shipping-order-item.model";
 
-export class ShippingOrderItemsView implements IParsable<ShippingOrderItemsView> {
+export class ShippingOrderItemsView implements IParsable<ShippingOrderItemsView>{
     public items: ShippingOrderItem[] = [];
 
     public parse(data: any): ShippingOrderItemsView {
         this.items = data.map((i: ShippingOrderItem) => new ShippingOrderItem().parse(i));
-        return this;
-    }
-
-}
-
-export class ShippingOrderItem implements IParsable<ShippingOrderItem> {
-    constructor(
-        public productId?: number,
-        public productUpc?: number,
-        public productName?: string,
-        public pickedQuantity?: number,
-        public currentQuantity?: number,
-        public quantity?: number,
-        public isPicked?: boolean,
-    ) {
-    }
-
-    public parse(data: any): ShippingOrderItem {
-        this.productId = data['productId'];
-        this.productUpc = data['productUpc'];
-        this.productName = data['productName'];
-        this.pickedQuantity = data['pickedQuantity'];
-        this.quantity = data['quantity'] || 0;
-        this.currentQuantity = data['currentQuantity'] || 0;
         return this;
     }
 }
